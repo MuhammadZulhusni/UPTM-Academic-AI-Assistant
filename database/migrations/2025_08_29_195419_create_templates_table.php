91ad0b8b9ab7135f78204d2fcba7b5df6ad45e1a
zlhsny <126148560+zulhusni2003@users.noreply.github.com>
2025-08-30 04:08:51 +0800

    Created migration for templates & TemplatInputFields

---
+++ database/migrations/2025_08_29_195419_create_templates_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('templates', function (Blueprint $table) {
+            $table->id(); 
+            $table->string('title'); 
+            $table->text('description'); 
+            $table->string('category'); 
+            $table->string('icon')->nullable(); // A small icon to represent the template. Can be empty.
+            $table->text('prompt'); // The main text or question that the template is based on.
+            $table->boolean('is_active')->default(true); // Determines if the template is currently available for use. It's active by default.
+            $table->foreignId('created_by')->constrained('users')->onDelete('cascade'); // Links the template to the user who created it. Deletes the template if the user is deleted.
+            $table->timestamps(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('templates');
+    }
+};
