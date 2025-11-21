3be2a7b2e83dffdaea534bdf7cc90df5b7ccff85
zlhsny <126148560+zulhusni2003@users.noreply.github.com>
2025-11-21 19:30:38 +0800

    docs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        {{-- Display the application name from config/app.php (fallback to "Laravel" if not set) --}}
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
